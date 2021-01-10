914a9153b4fd89a166470da48e7b4824d1889c2a
Tarik Guney <tarik.guney@motorolasolutions.com>
2021-01-09 22:54:05 -0700

    Added the new manager classes

---
+++ src/TarikGuney.ManagerAutomation.FunctionApp/Actors/PreviousIterationManager.cs
@@ -0,0 +1,12 @@
+using Akka.Actor;
+
+namespace TarikGuney.ManagerAutomation.Actors
+{
+	public class PreviousIterationManager: UntypedActor
+	{
+		protected override void OnReceive(object message)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ src/TarikGuney.ManagerAutomation.FunctionApp/Actors/ProgressReportManager.cs
@@ -0,0 +1,12 @@
+using Akka.Actor;
+
+namespace TarikGuney.ManagerAutomation.Actors
+{
+	public class ProgressReportManager: UntypedActor
+	{
+		protected override void OnReceive(object message)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
