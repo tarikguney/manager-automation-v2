030bd869199a40f53b570e190fe526e04688b7d1
Tarik Guney <tarik.guney@motorolasolutions.com>
2021-01-30 13:55:33 -0700

    Implementing the current iteration actors

--- src/TarikGuney.ManagerAutomation.FunctionApp/Managers/CurrentIterationManager.cs
+++ src/TarikGuney.ManagerAutomation.FunctionApp/Managers/CurrentIterationManager.cs
@@ -1,5 +1,5 @@
 using System;
-using System.ComponentModel.Design;
+using System.Collections.Generic;
 using Akka.Actor;
 using Akka.DI.Core;
 using Microsoft.Extensions.Options;
@@ -29,6 +29,23 @@ namespace TarikGuney.ManagerAutomation.Managers
 			_currentIterationInfoOptions = currentIterationInfoOptions;
 
 			Receive<StartAnalysisRequest>(StartAnalysis);
+			Receive<ActorResponse<IReadOnlyList<string>>>(HandleSubordinatesResponses);
+		}
+
+		private void HandleSubordinatesResponses(ActorResponse<IReadOnlyList<string>> response)
+		{
+			/* todo Handle the subordinate response
+				1. Count the number of responses received
+				2. Check if today's is the lsat day of the sprint, then count the number differently
+				3. If everything is received, then
+					a. Either form another message and send it to self.
+					b. Start sending the messages here directly
+				4. Choose the last day or regular days message sender
+				5. Push the messages to the message sender.
+				6. Stop self, which will terminate the child actors too.
+				Question: Will the actors run to the completion or do I need to wait for the response
+					in the program.cs file?
+			 */
 		}
 
 		private void StartAnalysis(StartAnalysisRequest request)
@@ -45,17 +62,26 @@ namespace TarikGuney.ManagerAutomation.Managers
 				Context.ActorOf(Context.DI().Props<EstimateWorkItemsActor>(), "estimate-work-item-actor");
 			var descriptiveTitleActor =
 				Context.ActorOf(Context.DI().Props<DescriptiveTitleActor>(), "descriptive-title-actor");
-			var activeWorkItemActor =
+			var activateWorkItemActor =
 				Context.ActorOf(Context.DI().Props<ActivateWorkItemActor>(), "activate-work-item-actor");
 			var descriptionActor = Context.ActorOf(Context.DI().Props<DescriptionActor>(), "description-actor");
 			var longCodeComplete = Context.ActorOf(Context.DI().Props<LongCodeCompleteActor>(), "long-code-complete-actor");
 			var greatWorkActor = Context.ActorOf(Context.DI().Props<GreatWorkActor>(), "great-work-actor");
+
 			var stillActiveWorkItemsActor = Context.ActorOf(Context.DI().Props<StillActiveWorkItemsActor>(),
 				"still-active-work-items-actor");
 
 			estimateWorkItemActor.Tell(currentIterationWorkItems);
 			descriptiveTitleActor.Tell(currentIterationWorkItems);
+			activateWorkItemActor.Tell(currentIterationWorkItems);
+			descriptionActor.Tell(currentIterationWorkItems);
+			longCodeComplete.Tell(currentIterationWorkItems);
+			greatWorkActor.Tell(currentIterationWorkItems);
 
+			if (_currentIterationInfoOptions.Value.FinishDate.Date == DateTime.Now.Date)
+			{
+				stillActiveWorkItemsActor.Tell(currentIterationWorkItems);
+			}
 		}
 	}
 
