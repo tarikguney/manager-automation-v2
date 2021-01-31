fd6a450a47d43f37b3381d854a61de295d0606eb
Tarik Guney <tarik.guney@motorolasolutions.com>
2021-01-30 22:21:00 -0700

    Implemented RetrospectiveManager and refrenced it in the function

---
+++ src/TarikGuney.ManagerAutomation.FunctionApp/Managers/RetrospectiveManager.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.DI.Core;
+using Microsoft.Extensions.Options;
+using TarikGuney.ManagerAutomation.Actors;
+using TarikGuney.ManagerAutomation.CommMessages;
+using TarikGuney.ManagerAutomation.IterationWorkItemRetrievers;
+using TarikGuney.ManagerAutomation.MessageSenders;
+using TarikGuney.ManagerAutomation.SettingsModels;
+
+namespace TarikGuney.ManagerAutomation.Managers
+{
+	public class RetrospectiveManager : ReceiveActor
+	{
+		private readonly IIterationWorkItemsRetriever _workItemsRetriever;
+		private readonly IRetrospectiveMessageSender _retrospectiveMessageSender;
+		private readonly IOptions<CurrentIterationInfo> _currentIterationInfoOptions;
+
+		public RetrospectiveManager(IIterationWorkItemsRetriever workItemsRetriever,
+			IRetrospectiveMessageSender retrospectiveMessageSender,
+			IOptions<CurrentIterationInfo> currentIterationInfoOptions
+			)
+		{
+			_workItemsRetriever = workItemsRetriever;
+			_retrospectiveMessageSender = retrospectiveMessageSender;
+			_currentIterationInfoOptions = currentIterationInfoOptions;
+
+			Receive<StartAnalysisRequest>(HandleStartAnalysisRequest);
+		}
+
+		private void HandleStartAnalysisRequest(StartAnalysisRequest obj)
+		{
+			// This is supposed to run on Mondays bi-weekly at the starting of the new iteration.
+			if (_currentIterationInfoOptions.Value.StartDate.Date != DateTime.Now.Date)
+			{
+				Context.Stop(Self);
+				Sender.Tell(new AnalysisCompleteResponse());
+			}
+
+			var currentIterationWorkItems =
+				_workItemsRetriever.GetWorkItems(IterationTimeFrame.Previous);
+
+			// Creating the subordinate actors.
+			var estimateWorkItemActor =
+				Context.ActorOf(Context.DI().Props<EstimateWorkItemsActor>(), "estimate-work-item-actor");
+			var descriptiveTitleActor =
+				Context.ActorOf(Context.DI().Props<DescriptiveTitleActor>(), "descriptive-title-actor");
+			var descriptionActor =
+				Context.ActorOf(Context.DI().Props<DescriptionActor>(), "description-actor");
+			var longCodeCompleteActor =
+				Context.ActorOf(Context.DI().Props<LongCodeCompleteActor>(), "long-code-complete-actor");
+			var greatPreviousIterationActor =
+				Context.ActorOf(Context.DI().Props<GreatPreviousIterationActor>(), "great-previous-iteration-actor");
+			var openWorkItemsActor =
+				Context.ActorOf(Context.DI().Props<OpenWorkItemsActor>(), "open-work-items-actor");
+
+			// Running the actors.
+			var tasks = new List<Task>();
+
+			var estimateWorkItemTask = estimateWorkItemActor
+				.Ask<ActorResponse<IReadOnlyList<string>>>(currentIterationWorkItems);
+			tasks.Add(estimateWorkItemTask);
+
+			var descriptiveTitleTask = descriptiveTitleActor
+				.Ask<ActorResponse<IReadOnlyList<string>>>(currentIterationWorkItems);
+			tasks.Add(descriptiveTitleTask);
+
+			var descriptionTask = descriptionActor
+				.Ask<ActorResponse<IReadOnlyList<string>>>(currentIterationWorkItems);
+			tasks.Add(descriptionTask);
+
+			var longCodeCompleteTask = longCodeCompleteActor
+				.Ask<ActorResponse<IReadOnlyList<string>>>(currentIterationWorkItems);
+			tasks.Add(longCodeCompleteTask);
+
+			var greatPreviousIterationTask = greatPreviousIterationActor
+				.Ask<ActorResponse<IReadOnlyList<string>>>(currentIterationWorkItems);
+			tasks.Add(greatPreviousIterationTask);
+
+			var openWorkItemsTask = openWorkItemsActor
+				.Ask<ActorResponse<IReadOnlyList<string>>>(currentIterationWorkItems);
+			tasks.Add(openWorkItemsTask);
+
+			// Waiting for all the of the actors to finish their work and return a response back.
+			Task.WaitAll(tasks.ToArray());
+
+			// Collecting the results from each actor.
+			var messages = new List<string>();
+			messages.AddRange(estimateWorkItemTask.Result.Content);
+			messages.AddRange(descriptiveTitleTask.Result.Content);
+			messages.AddRange(greatPreviousIterationTask.Result.Content);
+			messages.AddRange(openWorkItemsTask.Result.Content);
+			messages.AddRange(longCodeCompleteTask.Result.Content);
+
+			// Send the messages
+			_retrospectiveMessageSender.SendMessages(messages);
+
+			// Clearing out and exiting.
+			Context.Stop(Self);
+			Sender.Tell(new AnalysisCompleteResponse());
+		}
+	}
+}
--- src/TarikGuney.ManagerAutomation.FunctionApp/RetrospectiveAutomationFunction.cs
+++ src/TarikGuney.ManagerAutomation.FunctionApp/RetrospectiveAutomationFunction.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Threading.Tasks.Dataflow;
 using Akka.Actor;
 using Autofac;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Newtonsoft.Json.Linq;
 using TarikGuney.ManagerAutomation.AutoFacModules;
-using TarikGuney.ManagerAutomation.SettingsModels;
+using TarikGuney.ManagerAutomation.CommMessages;
+using TarikGuney.ManagerAutomation.Managers;
 
 namespace TarikGuney.ManagerAutomation
 {
@@ -30,54 +27,11 @@ namespace TarikGuney.ManagerAutomation
 
 			var actorSystem = ActorSystem.Create("retrospective-automation-actor-system");
 			actorSystem.UseAutofac(container);
-
-			var currentIteration = container.Resolve<IOptions<CurrentIterationInfo>>();
-
-			// Running this function every Monday and only send reminders if it is the beginning of the sprint.
-			if (currentIteration.Value.StartDate.Date != DateTime.Now.Date)
-			{
-				return;
-			}
-
-			/*var iterationWorkItemsTransformBlock = IterationWorkItemsRetrieverTransform.Block;*/
-			/*var googleChatMessageSender = PreviousIterationGoogleChatMessageSenderAction.Block;*/
-
-			/*var estimateWorkItemsTransformBlock = EstimateWorkItemsTransform.Block;
-			var descriptiveTitleTransformBlock = DescriptiveTitlesTransform.Block;
-			var descriptionTransformBlock = DescriptionTransform.Block;
-			var longCodeCompleteTransformBlock = LongCodeCompleteTransform.Block;
-			var greatPreviousIteration = GreatPreviousIteration.Block;
-			var openWorkItemsTransformBlock = OpenWorkItemsTransform.Block;*/
-
-			var broadcastBlock = new BroadcastBlock<List<JObject>>(null);
-			// Increase the limit of the batch size after adding another transform block.
-			var batchBlock = new BatchBlock<string>(6);
-
-			/*iterationWorkItemsTransformBlock.LinkTo(broadcastBlock);*/
-
-			/*broadcastBlock.LinkTo(estimateWorkItemsTransformBlock);
-			estimateWorkItemsTransformBlock.LinkTo(batchBlock);
-
-			broadcastBlock.LinkTo(descriptiveTitleTransformBlock);
-			descriptiveTitleTransformBlock.LinkTo(batchBlock);
-
-			broadcastBlock.LinkTo(descriptionTransformBlock);
-			descriptionTransformBlock.LinkTo(batchBlock);
-
-			broadcastBlock.LinkTo(longCodeCompleteTransformBlock);
-			longCodeCompleteTransformBlock.LinkTo(batchBlock);
-
-			broadcastBlock.LinkTo(greatPreviousIteration);
-			greatPreviousIteration.LinkTo(batchBlock);
-
-			broadcastBlock.LinkTo(openWorkItemsTransformBlock);
-			openWorkItemsTransformBlock.LinkTo(batchBlock);*/
-
-			/*batchBlock.LinkTo(googleChatMessageSender);
-
-			iterationWorkItemsTransformBlock.Post(IterationTimeFrame.Previous);
-			iterationWorkItemsTransformBlock.Complete();
-			await googleChatMessageSender.Completion;*/
+			var retrospectiveManager =
+				actorSystem.ActorOf(Props.Create<RetrospectiveManager>(), "retrospective-manager");
+			var result = retrospectiveManager.Ask(new StartAnalysisRequest(),
+				TimeSpan.FromMinutes(1));
+			result.Wait();
 		}
 	}
 }
